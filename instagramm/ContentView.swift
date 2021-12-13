967232483589f3cdff6285c3f694d61be19440a6
tupa1 <tupa1@runsystem.net>
2021-12-13 10:02:11 +0700

    Initial Commit

---
+++ instagramm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  instagramm
+//
+//  Created by GMO on 12/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ instagramm/instagrammApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagrammApp.swift
+//  instagramm
+//
+//  Created by GMO on 12/13/21.
+//
+
+import SwiftUI
+
+@main
+struct instagrammApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
